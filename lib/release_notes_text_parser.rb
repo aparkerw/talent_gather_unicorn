db1027ab4240d370a8d069c5a9570a95f0e2f647
Niaz Pour <npour@lat-npour.cb.careerbuilder.com careerbuilder.com>
2016-06-29 10:24:10 -0400

    added stuff kinda

--- lib/release_notes_text_parser.rb
+++ lib/release_notes_text_parser.rb
@@ -1,7 +1,8 @@
 class ReleaseNotesTextParser
 
   def self.get_version_sections(release_notes_text)
-    []
+    match =/(\d+\.\d+\.\d+) \*+(• .*\n)+/.match(release_notes_text)
+    match [1] if match
   end
 
   def self.get_version_number(version_section_text)
@@ -11,13 +12,16 @@ class ReleaseNotesTextParser
   end
 
   def self.get_feature_sections(version_section_text)
+  	match = //.match(version_section_text)
+  	match [1] if match
     # (feature changes|bug fixes|small tweaks)\n((• .*\n)+)?
     "this is wrong"
   end
 
   def self.get_feature_items(feature_section_items)
     #(• .*\n)+
-    "this is wrong"
+    match = /(• .*\n)+/.match(feature_section_text)
+    match[1] if match
   end
 
 end
