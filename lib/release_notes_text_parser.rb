fa984695a4da17508d46ebb78bb215e5eeb7cdfe
Adam Parker <aparkerw@gmail.com>
2016-06-29 10:01:28 -0400

    getting everything ready to implement, tests all rockin'

--- lib/release_notes_text_parser.rb
+++ lib/release_notes_text_parser.rb
@@ -3,7 +3,21 @@ class ReleaseNotesTextParser
   def self.get_version_sections(release_notes_text)
     []
   end
-  # (feature changes|bug fixes|small tweaks)\n((• .*\n)+)?
-  # \d+\.\d+\.\d+ \*+
+
+  def self.get_version_number(version_section_text)
+    # \d+\.\d+\.\d+ \*+
+    match = /(\d+\.\d+\.\d+) \*+/.match(version_section_text)
+    match[1] if match
+  end
+
+  def self.get_feature_sections(version_section_text)
+    # (feature changes|bug fixes|small tweaks)\n((• .*\n)+)?
+    "this is wrong"
+  end
+
+  def self.get_feature_items(feature_section_items)
+    #(• .*\n)+
+    "this is wrong"
+  end
 
 end
--- spec/release_notes_text_parser_spec.rb
+++ spec/release_notes_text_parser_spec.rb
@@ -1,10 +1,11 @@
 require 'release_notes_text_parser'
 
+
 RSpec.describe ReleaseNotesTextParser, '#get_version_sections' do
+  let(:sample_text) { File.read('spec/samples/release_notes_sample_full.txt') }
   context 'with version text' do
     it 'returns all the expected sections' do
       #expect the sample file to have given us 24 sections 
-      sample_text = File.read('spec/samples/release_notes_sample_full.txt')
       version_sections = ReleaseNotesTextParser.get_version_sections(sample_text)
       expect(version_sections.length).to eq 24
     end
@@ -17,56 +18,65 @@ RSpec.describe ReleaseNotesTextParser, '#get_version_sections' do
 end
 
 RSpec.describe ReleaseNotesTextParser, '#get_version_number' do
+  let(:sample_text_single) { File.read('spec/samples/release_notes_sample_small.txt') }
   context 'with valid version text' do
     it 'returns the version correctly' do
-      version_number = version_sections(sample_text).select {number}
-      expect version_number.first = "0.2.1"
-      # expect the version to be 0.3.0?
-      
+      version_number = ReleaseNotesTextParser.get_version_number(sample_text_single)
+      expect(version_number).to eq "0.3.0"
     end
   end
   context 'without version text' do
     it 'returns nil' do
-      #returns nil 
+      version_number = ReleaseNotesTextParser.get_version_number('empty string without version')
+      expect(version_number).to be_nil
     end
   end
 end
 
 RSpec.describe ReleaseNotesTextParser, '#get_feature_sections' do
+  let(:sample_text_single) { File.read('spec/samples/release_notes_sample_small.txt') }
   context 'with valid version text' do
     it 'returns 3 sections when 3 are given' do
-      feature_sections = ReleaseNotesTextParser.get_feature_sections(sample_text)
-      expect(feature_sections)= ("Features", "Bug Fixes", "Small Tweaks")
-      # expect the version to be 0.3.0?
+      feature_sections = ReleaseNotesTextParser.get_feature_sections(sample_text_single)
+      expect(feature_sections.length).to eq 3
     end
     it 'returns the correct 3 sections if given 3' do
-      # expect the version to be 0.3.0?
+      feature_sections = ReleaseNotesTextParser.get_feature_sections(sample_text_single)
+      expect(feature_sections.map {|section| section.name }).to eq ["Bug Fixes", "Small Tweaks", "Features"]
     end
     context 'bullet points' do
       it 'passes back bullet points when they exist' do
+        feature_sections = ReleaseNotesTextParser.get_feature_sections(sample_text_single)
+        expect(feature_sections.first.items.index('• Improved User Interface')).to eq 0
       end
       it 'passes back nil when there are no bullet points' do
+        feature_sections = ReleaseNotesTextParser.get_feature_sections('0.3.0 ***************\nFeatures\n\nBug Fixes\n\nSmall Tweaks')
+        expect(feature_sections.first.items).to be_nil
       end
     end
   end
   context 'without version section text' do
     it 'returns empty array' do
-      #returns []
+      feature_sections = ReleaseNotesTextParser.get_feature_sections('0.3.0 *************\n')
+      expect(feature_sections).to eq []
     end
   end
 end
 
 RSpec.describe ReleaseNotesTextParser, '#get_feature_items' do
+  let(:sample_text_items) { File.read('spec/samples/feature_items_list_sample.txt') }
   context 'with valid section body text' do
     it 'returns each line of text of the section when section is requested' do
-      sample_text = File.read('spec/samples/feature_items_list_sample.txt')
-      feture_items = ReleaseNotesTextParser.get_feature_items(sample_text)
-      expect(fetaure_items.lines).to eq 6
+      feature_items = ReleaseNotesTextParser.get_feature_items(sample_text_items)
+      expect(feature_items.length).to eq 6
     end
-    it 'returns an empty array if there are no feature lines'do
-  end
-      it 'doesn\'t return lines missing bullet points' do 
-      end 
+    it 'returns an empty array if there are no feature lines' do
+      feature_items = ReleaseNotesTextParser.get_feature_items('')
+      expect(feature_items.length).to be_nil
+    end
+    it 'doesn\'t return lines missing bullet points' do 
+      feature_items = ReleaseNotesTextParser.get_feature_items('line 1\n• line2\nline3')
+      expect(feature_items.length).to eq 1
+    end 
   end
 end
-
