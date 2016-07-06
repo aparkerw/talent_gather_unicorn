81932e9209ba6d732d076c3b516a7b576b39649c
Adam Parker <aparkerw@gmail.com>
2016-07-06 09:59:48 -0400

    function to actually parse our file!!!

--- lib/release_notes_to_html_converter.rb
+++ lib/release_notes_to_html_converter.rb
@@ -1,7 +1,25 @@
+require './release_notes_text_parser.rb'
+
 class ReleaseNotesToHtmlConverter
 
-  def hello_world(input = '')
+  def self.hello_world(input = '')
     "hello world! #{input.to_s}"
   end
 
+  def self.parse_release_notes
+    # load the file
+    content = File.read('../spec/samples/release_notes_sample_full.txt');
+    puts content;
+    # call parser 
+    # get the pieces
+    # turn them to html
+    # write the output
+    File.write('sample_output.txt', content);
+  end
+
 end
+
+
+# irb
+# require './release_notes_to_html_converter.rb'
+# ReleaseNotesToHtmlConverter.parse_release_notes
